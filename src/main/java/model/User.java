0e67178bd210a1dd7c3fc6414d0b3bd7b070281f
zhorzhevnaElena <zhorzhevna.elena@yandex.ru>
2025-03-20 00:22:10 +0300

    Реализован паттерн Builder для классов моделей

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,33 +5,58 @@ public class User {
     private String password;
     private String email;
 
-    public User(String name, String password, String email) {
+    private User(String name, String password, String email) {
         this.name = name;
         this.password = password;
         this.email = email;
+    }
+
+    public User() {
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+
+        public UserBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, password, email);
+        }
     }
 }
