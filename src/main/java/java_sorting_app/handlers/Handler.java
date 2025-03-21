a4fdba60f3dfaecc0e8fe4fe54446f6df505be82
Aleksey <mr.ryabov.aa@yandex.ru>
2025-03-21 18:37:29 +0300

    Перерабатывает вызов обработчиков пунктов меню

--- src/main/java/java_sorting_app/handlers/Handler.java
+++ src/main/java/java_sorting_app/handlers/Handler.java
@@ -3,17 +3,22 @@ package java_sorting_app.handlers;
 import java_sorting_app.model.Model;
 import java_sorting_app.view.Menu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Handler {
 
-    protected static Model model;
+    protected Map<Integer, Handler> menuMap;
 
-    protected Handler parentHandler;
+    protected static Model model;
 
     protected Menu menu;
 
     public Handler(Handler parentHandler, Menu menu) {
-        this.parentHandler = parentHandler;
         this.menu = menu;
+
+        menuMap = new HashMap<Integer, Handler>();
+        menuMap.put(0, parentHandler);
     }
 
     public static void setModel(Model model) {
@@ -24,9 +29,27 @@ public abstract class Handler {
         return menu.getMenuItems();
     }
 
-    public abstract Handler process(int numberMenu);
+    public Handler process(int numberMenu) {
+        Handler handler = getItemHandler(numberMenu);
+
+        if (handler == this) {
+            handle(numberMenu);
+        }
+
+        return handler;
+    }
+
+    protected abstract void handle(int numberMenu);
+
+    protected Handler getItemHandler(int numberMenu) {
+        if (!menuMap.containsKey(numberMenu)) {
+            return this;
+        }
+        return menuMap.get(numberMenu);
+    }
 
     public String getPWD(){
+        Handler parentHandler = menuMap.get(0);
         if(parentHandler != null){
             return parentHandler.getPWD() + "/" + menu.getMenuTitle();
         }
